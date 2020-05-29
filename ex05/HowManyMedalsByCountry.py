d5860f80cccf33d8b1346b767ac09ac2a2739450
RobinMazars <45461136+RobinMazars@users.noreply.github.com>
2020-05-29 14:41:52 +0200

    ex05

---
+++ ex05/HowManyMedalsByCountry.py
@@ -0,0 +1,19 @@
+from FileLoader import FileLoader
+from pprint import pprint
+
+
+def howManyMedalsByCountry(data, name):
+    full = data.query(f' Team == "{name}"')[["Year", "Medal"]]
+    d2 = full.groupby("Year").Medal
+    d3 = dict(d2.apply(list))
+    d4 = {}
+    for x in d3:
+        d4[x] = {'G': d3[x].count("Gold"), 'S': d3[x].count(
+            "Silver"), 'B': d3[x].count("Bronze")}
+    return d4
+
+
+loader = FileLoader()
+data = loader.load('../athlete_events.csv')
+re = howManyMedalsByCountry(data, 'France')
+pprint(re)
